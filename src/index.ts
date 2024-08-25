c634245555eafcc7bbeafef71b532a283a6e4997
Max Hale <maxhale@live.co.uk>
2024-08-25 17:30:44 +0100

    Added Rollup

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import Calendar from './Calendar';
+import type { CalendarProps, CalendarEvent, ViewType } from './types';
+
+export { Calendar };
+export type { CalendarProps, CalendarEvent, ViewType };
